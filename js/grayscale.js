a6372292eb0a4322f7c4c114df133a94944ec579
[abhish3k] <abhishekuniyal09@gmail.com>
2017-02-14 15:14:45 +0530

    Fixed Maps

--- js/grayscale.js
+++ js/grayscale.js
@@ -48,14 +48,14 @@ function init() {
         zoom: 15,
 
         // The latitude and longitude to center the map (always required)
-        center: new google.maps.LatLng(40.6700, -73.9400), // New York
+        center: new google.maps.LatLng(13.033130, 80.179608), // SRM University
 
         // Disables the default Google Maps UI components
-        disableDefaultUI: true,
+        disableDefaultUI: false,
         scrollwheel: false,
-        draggable: false,
+        draggable: true,
 
-        // How you would like to style the map. 
+        // How you would like to style the map.
         // This is where you would paste any style found on Snazzy Maps.
         styles: [{
             "featureType": "water",
@@ -167,7 +167,7 @@ function init() {
         }]
     };
 
-    // Get the HTML DOM element that will contain your map 
+    // Get the HTML DOM element that will contain your map
     // We are using a div with id="map" seen below in the <body>
     var mapElement = document.getElementById('map');
 
@@ -181,6 +181,5 @@ function init() {
         position: myLatLng,
         map: map,
         icon: image
-		zoom: 8
     });
 }
